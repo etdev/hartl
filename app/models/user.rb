d7fa61510b2cc31fcfddaba388d429e3a461471a
ericturnerdev <ericturnerdev@gmail.com>
2014-06-14 19:49:16 +0900

    Finish signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
+	before_create :create_remember_token #set the user's random remember_token
+
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :password, length: { minimum: 6 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,4 +10,22 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.digest(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+		#There's an indentation here because all methods after private keyword
+		#are private so you can remember that more easily
+		def create_remember_token
+			self.remember_token = User.digest(User.new_remember_token)
+		end
+	
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Hartl::Application.routes.draw do
   resources :users  #replaces get "users/new" 
+  resources :sessions, only: [:new, :create, :destroy] #restrict b/c we don't need show or edit
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
@@ -8,6 +9,8 @@ Hartl::Application.routes.draw do
 
  root 'static_pages#home'
  match '/signup', to: 'users#new', via: 'get'
+ match '/signin', to: 'sessions#new', via: 'get'
+ match '/signout', to: 'sessions#destroy', via: 'delete' #note the use of via: delete for signout
  match '/help', to: 'static_pages#help', via: 'get'
  match '/about', to: 'static_pages#about', via: 'get'
  match '/contact', to: 'static_pages#contact', via: 'get' 
