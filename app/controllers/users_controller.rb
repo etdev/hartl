6d17d66fd009ab268bd0e1daadab56f80c14cc20
ericturnerdev <ericturnerdev@gmail.com>
2014-06-16 23:56:28 +0900

    Added edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,26 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def new
   	@user = User.new
   end
 
   def create
-  	@user = User.new(user_params) #Not the final implementation
+  	@user = User.new(user_params)
   	if @user.save
   		#Handle a successful save
       sign_in @user
@@ -23,7 +37,7 @@ class UsersController < ApplicationController
   #Auxiliary method called user_params that returns an appropriate init. hash for 
   #the User model.  You don't test this.
   private
-  def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
