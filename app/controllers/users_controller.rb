0bf6bbb22b8911ee950b9b0b9f9816fff45ce6e8
ericturnerdev <ericturnerdev@gmail.com>
2014-06-17 17:26:38 +0900

    Chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,25 @@
 class UsersController < ApplicationController
   
+  #Check if signed in on these pages (signed_in_user is a method defined in this file)
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  #Check if the user is correct, when on the edit page (i.e. you can't edit someone else's info)
+  before_action :correct_user, only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
+
   def edit
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id]) #You no longer need this because the correct_user
+    #before filter defines @user.
   end
 
   def update
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id]) #no longer needed since we added the before filter
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -40,4 +50,27 @@ class UsersController < ApplicationController
     def user_params
     	params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    #Before filters
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
+
+
+
+
+
+
+
+
+
+
